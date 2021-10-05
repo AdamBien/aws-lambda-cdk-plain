d7ad8c1fb8aac563fb4a4258b9be6f390c8fdf7a
Adam Bien <abien@adam-bien.com>
2021-10-05 20:32:23 +0200

    type-dependent prefix removed for shorter ids

--- cdk/src/main/java/airhacks/CDKStack.java
+++ cdk/src/main/java/airhacks/CDKStack.java
@@ -16,12 +16,12 @@ public class CDKStack extends Stack {
     public CDKStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
         var function = createUserListenerFunction("airhacks_lambda_greetings_boundary_Greetings","airhacks.lambda.greetings.boundary.Greetings::onEvent", 128, 10);
-        CfnOutput.Builder.create(this, id("function-output")).value(function.getFunctionArn()).build();
+        CfnOutput.Builder.create(this, "function-output").value(function.getFunctionArn()).build();
     }
     
 
     Function createUserListenerFunction(String functionName,String functionHandler, int memory, int timeout) {
-        return Function.Builder.create(this, id(functionName))
+        return Function.Builder.create(this, functionName)
                 .runtime(Runtime.JAVA_11)
                 .code(Code.fromAsset("../target/function.jar"))
                 .handler(functionHandler)
@@ -31,7 +31,4 @@ public class CDKStack extends Stack {
                 .build();
     }
     
-    String id(String type) {
-        return String.format("%s-%s", this.getArtifactId(), type);
-    }
 }
